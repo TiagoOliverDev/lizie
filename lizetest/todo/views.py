061d784f6c41ec18bcc5ea0db6d772546b2e3c33
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-30 22:16:43 -0300

    feat: Desafio 10

--- lizetest/todo/views.py
+++ lizetest/todo/views.py
@@ -7,6 +7,7 @@ from .forms import TaskForm, CategoryForm, CommentForm
 from .task_filters import TaskFilter
 from .category_filters import CategoryFilter
 from django_filters.views import FilterView
+from django.http import HttpResponseForbidden
 
 class TaskListView(LoginRequiredMixin, FilterView):
     model = Task
@@ -74,4 +75,16 @@ class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     template_name = 'task_confirm_delete.html'
     success_url = reverse_lazy('todo:task_list')
+
+    def form_valid(self, form):
+        if self.object.completed:
+            context = self.get_context_data()
+            context['error'] = "Cannot delete a completed task."
+            return self.render_to_response(context)
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_completed'] = self.object.completed
+        return context
 
