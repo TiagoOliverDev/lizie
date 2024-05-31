0b83f0c4e1d69483ad76d9e46161da1707234b93
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-05-31 16:32:17 -0300

    feat: ajuste label e pagination qtd

--- lizetest/todo/category_filters.py
+++ lizetest/todo/category_filters.py
@@ -2,7 +2,7 @@ import django_filters
 from .models import Category
 
 class CategoryFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains')
+    name = django_filters.CharFilter(lookup_expr='icontains', label='Search by category name')
 
     class Meta:
         model = Category
--- lizetest/todo/views.py
+++ lizetest/todo/views.py
@@ -15,7 +15,7 @@ class TaskListView(LoginRequiredMixin, FilterView):
     filterset_class = TaskFilter
     template_name = 'task_list.html'
     context_object_name = 'tasks'
-    paginate_by = 5
+    paginate_by = 7
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
