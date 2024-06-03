248f0382129124aaec11074addd102a1e7240931
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-06-02 22:19:33 -0300

    feat: configurando banco de testes

--- lizetest/settings.py
+++ lizetest/settings.py
@@ -85,9 +85,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },    
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',  
     }
 }
 
+# DATABASES['default'] = DATABASES['test']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
